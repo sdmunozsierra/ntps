c581ab3607efadded8e6ba28c1121978a7d90259
isaigonz48 <isaigonz48@gmail.com>
2019-04-26 13:20:35 -0600

    starting stuff

---
+++ Field.py
@@ -0,0 +1,7 @@
+class Field:
+    def __init__(self, name, value, mask, displayFormat, selected):
+        self.name = name
+        self.value = value
+        self.mask = mask
+        self.displayFormat = displayFormat
+        self.selected = selected
