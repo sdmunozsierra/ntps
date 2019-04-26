a4ef001974aa6194bf61737e3dfa90739ac4050a
isaigonz48 <isaigonz48@gmail.com>
2019-04-26 14:04:56 -0600

    First version of queue

--- Queue.py
+++ Queue.py
@@ -1,1 +1,39 @@
+#
+# Stuff
+#
+
 class Queue:
+    packets = list()
+    size = 100
+    
+    def __init__(self, size):
+        self.size = size
+
+    def setSize(self, newSize):
+        self.size = newSize
+
+    def forwardPacket(self, packet):
+        index = packets.index(packet)
+        if(index == 0):
+            InterceptionInterface.sendPacket(packets.pop([0]))
+        else:
+            while(index >= 0):
+                InterceptionInterface.sendPacket(packets.pop([0]))
+                index = index - 1
+
+    def dropPacket(self, packet):
+        packets.remove(packet)
+
+    def addPacket(self, packet):
+        if(len(packets) >= size):
+            return
+        else:
+            packets.append(packet)
+
+    def getPackets(self):
+        return this.packets
+
+    def getSize(self):
+        return this.size
+
+    
