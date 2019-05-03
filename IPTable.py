791a95bd607ba19030e85674c8ca5cd2b281532a
jdelacruz5 <jdelacruz@miners.utep.edu>
2019-05-03 13:11:35 -0600

    updated and hopefully completed iptable class

--- IPTable.py
+++ IPTable.py
@@ -1,4 +1,6 @@
-from subprocess import call
+import fnfqueue
+from scapy.all import *
+import os
 class IPTable:
      
     def __init__(self):
@@ -6,12 +8,14 @@ class IPTable:
 
     def toggleIPTableState(self):
         if (self.status == False):
-            #add proper IPTable function to turn on IPTable this could be the wrong command still not sure
-            call('systemctl enable ufw', shell = True)
+            # set the IPtable rules -I is to insert the rule -d is the destination -j to jump if it matches rule to NFQUEUE is
+            rule = "iptables -I INPUT -d 192.168.0.0/24 -j NFQUEUE --queue-num 1" 
+            os.system(rule)
             self.status = True
         if (self.status == True):
-            #add proper IPTable function to turn off IPTable this could be the wrong command still not sure
-            call('systemctl disable ufw', shell = True)
+            # Turn the IPtable off -F is to delete all the current rules -X is to delete all the user defined rules
+            os.system('iptables -F')
+            os.system('iptables -X')
             self.status = False
 
     def getStatus(self):
