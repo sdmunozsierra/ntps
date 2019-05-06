360449e0a5c4b91060aeb049b65dab9c510ce7a7
jdelacruz5 <jdelacruz@miners.utep.edu>
2019-05-06 16:55:46 -0600

    Fixed minor errors, still need to add the packet handling function

--- Interface/ip_table.py
+++ Interface/ip_table.py
@@ -2,7 +2,7 @@ from netfilterqueue import NetfilterQueue
 from scapy.all import *
 import os
 class IPTable:
-    nfq= NetfilterQueue()
+    nfq = NetfilterQueue()
 
     def __init__(self):
         self.status = False
@@ -10,7 +10,7 @@ class IPTable:
     def toggleIPTableState(self):
 
         if (self.status == False):
-            # set the IPtable rules -A is to append a new rule to the exisiting rules  -j to jump if it matches rule to NFQUEUE is
+            # Set the IPtable rules -A is to append a new rule to the exisiting rules  -j to jump if it matches rule to NFQUEUE is
             rule = "iptables -A OUTPUT -j NFQUEUE --queue-num 1" 
             os.system(rule)
             self.nfq.bind(1, "new packet function")
