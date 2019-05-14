b224462a9cd3b6abea7e94bbf76f96c311fe15d3
isaigonz48 <isaigonz48@gmail.com>
2019-05-13 19:00:37 -0600

    Adjusted save and load functions

--- Packet/pcap.py
+++ Packet/pcap.py
@@ -2,11 +2,12 @@
 PCAP class will provide its information to the classes that need it.
 """
 import os
-
+from scapy.all import *
 
 class PCAP:
     def __init__(self, name, timestamp):
         self.packets = list()
+        self.testpackets = list()
         self.name = name
         self.timestamp = timestamp
         self.load()
@@ -22,16 +23,64 @@ class PCAP:
         if(not os.path.exists("./PCAPs")):
             os.makedirs("./PCAPs")
 
-        filepath = "{}{}{}".format("/PCAPs/", self.name, ".txt")
+        filepath = "{}{}{}".format("/PCAPs/", self.name, ".pcap")
 
-        file = open(filepath, 'w')
-        file.write("Hello")
-        file.close()
+        wrpcap(filepath, self.packets)
+        #file = open(filepath, 'w')
+        #file.write("Hello")
+        #file.close()
 
     def load(self):
         # TODO: Transform info to packets
+        pcapfile = rdpcap('test3.pcapng')
+        i = 1
+        for packet in pcapfile:
+            self.packets.append(packet)
+
+        i = 0
+        test = list()
+        #self.packets[1].show()
+        while i < len(self.packets):
+            test.append(list(self.expand(self.packets[i])))
+            i += 1
+
+        #print(test[0][0].name)
+        i = 0
+        while i < len(self.packets):
+            pktname = "Frame " + str(i+1) + ": "
+            for layer in test[i]:
+                pktname += layer.name + " "
+            print(pktname)
+
+            testpkt = Packet(pktname, test[i])
+            #print(testpkt.getName())
+            self.testpackets.append(testpkt)
+            
+            i += 1
+
+        #print("?")    
         pass
 
+    def expand(self,x):
+        yield x
+        while x.payload:
+            #print(x.payload)
+            x = x.payload
+            yield x
+        print("!")
 
 pcap = PCAP("test", 0)
-pcap.save()
+#pcap.save()
+#pcap.load()
+
+
+
+#for packet in pcap.packets:
+#    for layer in packet:
+        
+    
+    
+    
+#packets = sniff(iface = conf.iface, count = 10)
+#for pkt in packets:
+#    pkt.show()
