97566be06696921073b6c3a238c0caf8b8f1c0ac
jdelacruz5 <jdelacruz@miners.utep.edu>
2019-05-16 21:12:37 -0600

    update iptable class

--- Interface/ip_table.py
+++ Interface/ip_table.py
@@ -2,7 +2,6 @@ from netfilterqueue import NetfilterQueue
 from scapy.all import *
 import os
 class IPTable:
-    nfq = NetfilterQueue()
 
     def __init__(self):
         self.status = False
@@ -11,15 +10,12 @@ class IPTable:
 
         if (self.status == False):
             # Set the IPtable rules -A is to append a new rule to the exisiting rules  -j to jump if it matches rule to NFQUEUE is
-            rule = "iptables -A OUTPUT -j NFQUEUE --queue-num 1" 
+            rule = "iptables -A INPUT -j NFQUEUE --queue-num 1" 
             os.system(rule)
-            self.nfq.bind(1, "new packet function")
-            self.nfq.run(block=True)
             self.status = True
         if (self.status == True):
             # Turn the IPtable off -D is to delete the current rules
-            self.nfq.unbind()
-            delete_rule= "iptables -D OUTPUT -j NFQueue --queue-num 1"
+            delete_rule = "iptables -D INPUT -j NFQueue --queue-num 1"
             os.system(delete_rule)
             self.status = False
 
