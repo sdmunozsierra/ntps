46577adb8af0a76de98975e6e5e5a86a90996c28
jdelacruz5 <jdelacruz@miners.utep.edu>
2019-05-17 01:42:12 -0600

    add stuff

--- Interface/interception_interface.py
+++ Interface/interception_interface.py
@@ -10,10 +10,16 @@ class InterceptionInterface:
     def toggleIntercept(self):
         if (self.intercepting == False):
             self.nfqueue = NetfilterQueue()
-            self.nfqueue.bind(1)
+            self.nfqueue.bind(1,self.display)
             self.intercepting = True
             print("Interception has been turned on.")
         else:
             self.nfqueue.unbind()
             print("Inteception has been turned off.")
             self.intercepting = False
+
+    def display(self, packet):
+        print(packet)
+        packet = IP(packet.get_payload())
+        print(type(packet))
+        self.packet_manager.addToQueue(packet)
